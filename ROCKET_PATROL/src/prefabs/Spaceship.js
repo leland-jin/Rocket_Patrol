4841dcf431c6219e2774a83c24d8af9e05149743
Leland Jin <nli32@ucsc.edu>
2021-04-13 02:47:21 +0800

    Debugging...

--- ROCKET_PATROL/src/prefabs/Spaceship.js
+++ ROCKET_PATROL/src/prefabs/Spaceship.js
@@ -11,7 +11,7 @@ class Spaceship extends Phaser.GameObjects.Sprite {
         // move spaceship left
         this.x -= this.moveSpeed;
         // wrap around from left edge to right edge
-        if(this.x <= 0 - this.width) {
+        if (this.x <= 0 - this.width) {
             this.reset();
         }
     }
